398761a44175d47ac49e9423d2b2c758161f5318
Joel Kosola <joel.kosola@eatech.fi>
2020-02-14 17:36:54 +0200

    Added migrations

---
+++ WBot/WBot.DataAccess/Migrations/20200214153105_timestamp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WBot.DataAccess.Migrations
+{
+    public partial class timestamp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FirstPostTimeStamp",
+                table: "Links",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstPostTimeStamp",
+                table: "Links");
+        }
+    }
+}
